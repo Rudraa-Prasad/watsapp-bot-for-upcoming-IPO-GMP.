fb9198eca79adb86bcd07cf96ec171f833083e85
rudra <pdrudra.121@gmail.com>
2024-09-16 00:04:39 +0530

    Initial c

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ GOOGLE_GENAI_API_KEY = os.getenv("GOOGLE_GENAI_API_KEY")
 TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
 TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+genai.configure(api_key=GOOGLE_GENAI_API_KEY)
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 app = FastAPI()
